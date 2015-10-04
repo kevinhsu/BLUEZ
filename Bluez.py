f80c57f57c2a3631170df5cb04b4ded6827e6c90
Shaid Khan <ShaidK@Outlook.com>
2015-10-04 12:31:36 +0100

    UPDATED BLUEZ FILE

--- Bluez.py
+++ Bluez.py
@@ -10,26 +10,32 @@ from bluetooth import *;
 
 class Bluez:
     " Note: Simple Class To Connect To Bluetooth Devices "
+    " Todo: Specify Port For Socket Connection "
     PORT = 1;
     
     def __init__(self, device_name = "Default Devices"):
-        " Note: Constructor Method To Initialise Bluez "
-        " @param: device_name = Connecting Device Name "
+        """ Note: Constructor Method To Initialise Bluez 
+            @param: device_name = Connecting Device Name """
         # Note: _mac Used To Store Device Mac Address
         self._name = device_name;
         self._mac = None;
 
-    @property
-    def _mac(self):
-        " Note: Return Store Mac Address "
-        return self._mac;
-
     def __str__(self):
-        " Note: Method To Return String Instance "
+        """ Note: Method To Return String Instance """
         return "Device: " + self._name;
 
-    def _search(self):
-        " Note: Method To Scan For Nearby Devices "
+    @property
+    def name(self):
+        """ Note: Return Store Name Of Device """
+        return self._name;
+
+    @property
+    def mac(self):
+        """ Note: Return Store Mac Address """
+        return self._mac;
+
+    def search(self):
+        """ Note: Method To Scan For Nearby Devices """
         try:
             devices = discover_devices(8, True, True);
             if (len(devices) > 0):
@@ -40,8 +46,8 @@ class Bluez:
             self.__message(0);
 
     def __confirm(self, arr):
-        " Note: Method To Confirm The Device "
-        " @param: arr = List Of Devices "
+        # Note: Method To Confirm The Device 
+        # @param: arr = List Of Devices "
         for device in arr:
             if (self._name == device[1]):
                 if (is_valid_address(x[0])):
@@ -51,12 +57,12 @@ class Bluez:
                     self.__message(1);
 
     def __connect(self):
-        " Note: Method To Connect To Device "
+        # Note: Method To Connect To Device "
         sock = BluetoothSocket(RFCOMM);
         sock.connect(self._mac, PORT);
         
     def __messages(self, num):
-        " Note: Method To Display Messages "
+        # Note: Method To Display Messages "
         if num == 0:
             print " Check Bluetooth Module ";
         if num == 1:
