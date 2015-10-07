b3d0ef5b67801557cb6539b1cb47af71537a35d9
Shaid Khan <ShaidK@Outlook.com>
2015-10-07 11:14:54 +0100

    FIX BUGS WITH NAMING ERRORS

--- Bluez.py
+++ Bluez.py
@@ -10,19 +10,18 @@ from bluetooth import *;
 
 class Bluez:
     " Note: Simple Class To Connect To Bluetooth Devices "
-    " Todo: Specify Port For Socket Connection "
-    PORT = 1;
     
-    def __init__(self, device_name = "Default Devices"):
+    def __init__(self, device_name = "Default Devices", port = 1):
         """ Note: Constructor Method To Initialise Bluez 
-            @param: device_name = Connecting Device Name """
+            @param: device_name = Connecting Device Name
+            'param: port :: Socket Port Number """
         # Note: _mac Used To Store Device Mac Address
         self._name = device_name;
         self._mac = None;
 
     def __str__(self):
         """ Note: Method To Return String Instance """
-        return "Device: " + self._name;
+        return "Device: " + self._name + self._mac;
 
     @property
     def name(self):
@@ -34,32 +33,45 @@ class Bluez:
         """ Note: Return Store Mac Address """
         return self._mac;
 
+    def quick_s(self):
+        """ Note: Method To Quickly Scan For Devices """
+        # Note: Quickly Use This To Search Fr Device
+        try:
+            devices = discover_devices(8, True, True);
+            if (len(devices) > 0):
+                print devices;
+        except IOError:
+            self.__messages(0);
+
     def search(self):
         """ Note: Method To Scan For Nearby Devices """
         try:
             devices = discover_devices(8, True, True);
             if (len(devices) > 0):
                 self.__confirm(devices);
+                print devices;
             else:
-                self.__message(1);
+                self.__messages(1);
         except IOError:
-            self.__message(0);
+            self.__messages(0);
 
     def __confirm(self, arr):
         # Note: Method To Confirm The Device 
         # @param: arr = List Of Devices "
         for device in arr:
             if (self._name == device[1]):
-                if (is_valid_address(x[0])):
-                    self._mac = x[0];
-                    __connect();
+                if (is_valid_address(device[0])):
+                    self._mac = device[0];
+                    self.__messages(1)
+                    self.__connect();
                 else:
                     self.__message(1);
 
     def __connect(self):
         # Note: Method To Connect To Device "
         sock = BluetoothSocket(RFCOMM);
-        sock.connect(self._mac, PORT);
+        sock.connect((self._mac, 1));
+        self.__str__()
         
     def __messages(self, num):
         # Note: Method To Display Messages "
@@ -69,6 +81,8 @@ class Bluez:
             print " Device Not Found ";
         if num == 2:
             print " Device Found ";
-    
-   
-    
+
+ 
+if __name__ == "__main__":
+    bluez = Bluez("YOUR DEVICE NAME")
+    bluez.search()
